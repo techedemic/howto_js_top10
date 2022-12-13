8a1da05aa4688dc9fe424ee70aac0cef6f4b28ba
Hendri <108517230+hendri-schoeman@users.noreply.github.com>
2022-12-13 09:44:43 +0200

    Added how to convert string to a number

---
+++ 5_how_to_convert_string_to_number.js
@@ -0,0 +1,27 @@
+// const startNumber = "12.5";
+// console.log("Before conversion ->", typeof startNumber);
+
+// let convertedNumber;
+// convertedNumber = Number(startNumber);
+// convertedNumber = parseInt(startNumber, 2);
+// convertedNumber = parseFloat(startNumber);
+
+// console.log(startNumber, convertedNumber);
+// console.log("After conversion ->", typeof convertedNumber);
+
+// #f21b0c
+// const R = parseInt("f2", 16);
+// const G = parseInt("1b", 16);
+// const B = parseInt("0c", 16);
+
+// console.log(`${R},${G},${B}`);
+
+console.log(parseInt("100", 2));
+
+/**
+ * 
+ * Common numbering systems:
+ binary - 0 1
+ decimal - 0 1 2 3 4 5 6 7 8 9
+ hexadecimal - 0 1 2 3 4 5 6 7 8 9 A B C D E F #ff00ee
+ */
