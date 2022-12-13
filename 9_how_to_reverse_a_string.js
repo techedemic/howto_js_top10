e16aabb40fb8265c907f80805424e0b4108d00ef
Hendri <108517230+hendri-schoeman@users.noreply.github.com>
2022-12-13 10:06:48 +0200

    How to reverse a string

---
+++ 9_how_to_reverse_a_string.js
@@ -0,0 +1,9 @@
+const testString = "Greetings from Cape Town";
+
+// Use array functions
+
+const splitString = testString.split("");
+const reverseArray = splitString.reverse();
+const reverseString = reverseArray.join("");
+
+console.log(reverseString);
