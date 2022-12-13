3bfd9bcf76fceee0e4a85e8349e4021d4a831af3
Hendri <108517230+hendri-schoeman@users.noreply.github.com>
2022-12-13 10:11:11 +0200

    How to concatenate strings

---
+++ 10_how_to_concatenate_strings.js
@@ -0,0 +1,21 @@
+const str1 = "Hello";
+const str2 = "World";
+
+// console.log(str1 + " " + str2);
+// console.log(`${str1} fajksldhflksjdf ${str2}`);
+
+const str3 = str1.concat(" ", str2);
+console.log(str3);
+
+// const concat3 = "John".concat(
+//     " ",
+//     "Doe",
+//     " ",
+//     "is",
+//     " ",
+//     "30",
+//     " ",
+//     "years",
+//     " ",
+//     "old"
+//   );
