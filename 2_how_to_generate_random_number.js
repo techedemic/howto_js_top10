99231ab6ad918942036669b7b42930f8b335d3a4
Hendri <108517230+hendri-schoeman@users.noreply.github.com>
2022-12-13 09:15:44 +0200

    added generate random number

---
+++ 2_how_to_generate_random_number.js
@@ -0,0 +1,10 @@
+// 1. Generate a single random number
+//const number = Math.random() * 10;
+
+// console.log(Math.floor(number));
+
+// 2. Generate a 100 random numbers
+for (let index = 0; index < 100; index++) {
+  const number = Math.random() * 100;
+  console.log(Math.floor(number));
+}
