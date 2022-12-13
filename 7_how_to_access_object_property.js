f8de8a3495b2dbc034f8addeb3b541cedf8d953a
Hendri <108517230+hendri-schoeman@users.noreply.github.com>
2022-12-13 09:51:42 +0200

    How to access object properties

---
+++ 7_how_to_access_object_property.js
@@ -0,0 +1,13 @@
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+};
+
+// console.log(person);
+
+// Lastname method 1
+console.log("Last name", person["lastName"]);
+
+// Lastname method 2
+console.log("Last name", person.lastName);
